f604a13fe5eeeb97a913b74e1d3265e8c53166de
Tibor Toth <tothti@yahoo.com>
2020-04-27 12:33:44 +0200

    modify hello.py add training.py

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
 #!/bin/python
 
-print("Hello World")
+
+def main():
+    print("Hello World")
+
+if __name__ == '__main__':
+    main()
---
+++ training1.py
@@ -0,0 +1,11 @@
+
+def contact_card(name, phone, address, gender='Male'):
+    print(f"Name is: {name}")
+    print(f"Phone is: {phone}")
+    print(f"Address is: {address}")
+    print(f"Gender is: {gender}")
+
+
+contact_card('Tibi', 123, gender='Male', address='cím')
+
+
